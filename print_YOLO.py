900f96561eab4d1d293ba7aefb65cede8f910420
AngelababyTing <32257876+AngelababyTing@users.noreply.github.com>
2018-03-21 19:54:10 +0800

    Add files via upload

---
+++ print_YOLO.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import shutil
+
+input = sys.argv[1]
+filecount = 1
+
+
+#列出輸入參數位置底下的文件
+os.mkdir("YOLO")
+
+for filename in os.listdir(input):
+	os.system("E:\darknet\\build\darknet\\x64\darknet.exe detect cfg\yolo.cfg weights\yolo.weights " + input + "\\" + filename)
+	os.rename("predictions.jpg", str(filecount) + ".jpg")
+	shutil.move(str(filecount) + ".jpg","YOLO")
+	filecount += 1
